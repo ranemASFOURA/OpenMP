c8a97be4f153f11ae3d836ac81b10c76812582e4
ranemASFOURA <ranem.y.asfoura@gmail.com>
2024-11-03 19:03:18 +0000

    Improved performance

--- first_question/log_integration.c
+++ first_question/log_integration.c
@@ -2,9 +2,36 @@
 #include <omp.h>
 #include <math.h>
 
-// Function to calculate f(x) = ln(x) / x
 double function(double x) {
     return log(x) / x;
+}
+
+// Function to perform numerical integration
+double integrate(int N, double a, double b, int num_threads) {
+    double width = (b - a) / N;       // Width of each rectangle
+    double total_area = 0.0;          // Variable to store the total area
+
+   
+    omp_set_num_threads(num_threads);
+
+    // Parallel region with reduction
+    #pragma omp parallel reduction(+:total_area)
+    {
+        // Local variable for each thread
+        double partial_area = 0.0;
+
+        // Parallel loop with dynamic schedule
+        #pragma omp for schedule(dynamic)
+        for (int i = 0; i < N; i++) {
+            double x = a + (i + 0.5) * width;  // Midpoint for better accuracy
+            partial_area += function(x) * width; 
+        }
+
+       
+        total_area += partial_area; // This will be handled by the reduction
+    }
+
+    return total_area;
 }
 
 int main() {
@@ -12,40 +39,15 @@ int main() {
     double b = 10.0; // End of the interval
     int N;           // Variable to hold user input for N
 
-    // Prompt user for input
+    
     printf("Enter the number of rectangles (N): ");
-    scanf("%d", &N);  // Read the value of N from user input
+    scanf("%d", &N);  
 
-    double width = (b - a) / N;  // Width of each rectangle
-    double total_area = 0.0;      // Variable to store the total area
-
-    // Parallel region
-    #pragma omp parallel
-    {
-        // Get the number of threads used
-        int num_threads = omp_get_num_threads();
-        
-        // Print the number of threads (only in the master thread)
-        #pragma omp master
-        {
-            printf("Number of threads used: %d\n", num_threads);
-        }
-
-        // Local variable for each thread
-        double partial_area = 0.0;  
-
-        // Parallel loop
-        #pragma omp for
-        for (int i = 0; i < N; i++) {
-            double x = a + (i + 0.5) * width;  // Midpoint for better accuracy
-            partial_area += function(x) * width; // Accumulate partial area for each thread
-        }
-
-        // Accumulate the partial areas into total_area
-        #pragma omp atomic
-        total_area += partial_area;
+    // Loop over different thread counts
+    for (int num_threads = 2; num_threads <= 8; num_threads *= 2) {
+        double total_area = integrate(N, a, b, num_threads);
+        printf("For N = %d and using %d threads, the numerical integration of ln(x) / x over [1,10] is: %.10f\n", N, num_threads, total_area);
     }
 
-    printf("For N = %d, the numerical integration of ln(x) / x over [1,10] is: %.10f\n", N, total_area);
     return 0;
 }
